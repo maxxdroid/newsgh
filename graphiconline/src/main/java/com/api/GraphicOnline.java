555712a5c966ac1836642b48b3380536e674f54c
maxxdroid <theonlymaxmc@gmail.com>
2025-04-17 01:05:59 -1200

    Remove old Main classes and rename GhanaWebApplication to GhanaWeb
    - Delete Main.java files from graphiconline and myjoyonline projects.
    - Rename GhanaWebApplication.java to GhanaWeb.java and update the main method reference.
    - Introduce new MyJoyOnline.java and GraphicOnline.java with Spring Boot application setup.

--- graphiconline/src/main/java/com/api/GraphicOnline.java
+++ graphiconline/src/main/java/com/api/GraphicOnline.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GhanaWebApplication {
+public class GraphicOnline {
     public static void main(String[] args) {
-        SpringApplication.run(GhanaWebApplication.class, args);
+        SpringApplication.run(GraphicOnline.class, args);
     }
 }
